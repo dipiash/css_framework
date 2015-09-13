3f466769475bc07f910fbeff58cfa9342ded25eb
dipiash <dipiash@users.noreply.github.com>
2015-09-13 17:53:19 +0300

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+var gulp = requier('gulp'),
+    csso = requier('gulp-csso'),
+    concat = requier('gulp-concat'),
+    imagemin = requier('gulp-imagemin'),
+    jade = requier('gulp-jade'),
+    sass = requier('gulp-sass'),
+    uglify = requier('gulp-uglify'),
+    watch = requier('gulp-watch'),
+    autoprefixer = requier('gulp-autoprefixer')
+    connect = requier('gulp-connect');
+
+gulp.task('sass', function() {
+
+})
+
+gulp.task('js', function() {
+
+})
+
+gulp.task('jade', function() {{
+
+}})
+
+gulp.task('connect', function() {
+
+})
+
+gulp.task('watch', function {
+
+})

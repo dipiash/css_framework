6e4b25061df580adbf0058f42a21759d32048eee
dipiash <dipiash@users.noreply.github.com>
2015-09-14 18:32:13 +0300

    Added sass-task and watch-task for sass files

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,39 @@
-var gulp = requier('gulp'),
-    csso = requier('gulp-csso'),
-    concat = requier('gulp-concat'),
-    imagemin = requier('gulp-imagemin'),
-    jade = requier('gulp-jade'),
-    sass = requier('gulp-sass'),
-    uglify = requier('gulp-uglify'),
-    watch = requier('gulp-watch'),
-    autoprefixer = requier('gulp-autoprefixer')
-    connect = requier('gulp-connect');
+var gulp = require('gulp'),
+    csso = require('gulp-csso'),
+    concat = require('gulp-concat'),
+    imagemin = require('gulp-imagemin'),
+    jade = require('gulp-jade'),
+    sass = require('gulp-sass'),
+    uglify = require('gulp-uglify'),
+    watch = require('gulp-watch'),
+    autoprefixer = require('gulp-autoprefixer')
+    connect = require('gulp-connect');
 
 gulp.task('sass', function() {
-
+  gulp.src('sass/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(concat('style.css'))
+    .pipe(csso())
+    .pipe(autoprefixer({
+            browsers: ['> 1%'],
+            cascade: false
+        }))
+    .pipe(gulp.dest('dist/css'));
 })
 
+/*
 gulp.task('js', function() {
 
 })
-
+*/
 gulp.task('jade', function() {{
 
 }})
-
+/*
 gulp.task('connect', function() {
 
 })
-
-gulp.task('watch', function {
-
-})
+*/
+gulp.task('watch',function() {
+    gulp.watch('sass/*.scss',['sass']);
+});
